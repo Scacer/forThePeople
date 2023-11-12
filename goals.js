6563e14fbcb0c13703d6dd1a41a1f6b1faa67ef1
Scacer <60669273+Scacer@users.noreply.github.com>
2023-11-12 11:40:33 +0000

    let there be light!
    - added the files "index.html", "goals.js" and "forThePeople.css" as the homepage, javascript for nav bar, and stylesheet respectively.

---
+++ goals.js
@@ -0,0 +1,7 @@
+function toggleGoalNav() {
+    let links = document.querySelector('.subNav');
+    links.classList.toggle('showSubNav')
+}
+
+let goalsNav = document.querySelector('.goalsNav');
+goalsNav.addEventListener("click", toggleGoalNav);
